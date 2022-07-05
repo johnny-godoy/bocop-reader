1f19c32774fb8047c08a9a3d1c75e3e919317b51
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-07-04 23:46:02 -0400

    fix several typos

--- bocop_solution.py
+++ bocop_solution.py
@@ -9,7 +9,7 @@ import pandas as pd
 from models._bunches import _AdjointStates, _Controls, _States
 
 warnings.filterwarnings("error", category=UserWarning)
-__CONSTANT_NAMES: tuple[str, str, str] = ("discretization_times", "stage_times", "parameters")
+_CONSTANT_NAMES = ("discretization_times", "stage_times", "parameters")
 
 
 class BOCOPSolution:
@@ -41,13 +41,13 @@ class BOCOPSolution:
         working_directory_filename: str
             Name of the folder which contains the solution files."""
         self.working_directory_filename = working_directory_filename
-        for name in __CONSTANT_NAMES:
+        for name in _CONSTANT_NAMES:
             setattr(self, name, self.file_to_array(name))
         files = []
         for file in os.listdir(working_directory_filename):
             filename = os.fsdecode(file)
             prefix, suffix = filename.split(".")
-            if suffix == "export" and prefix not in __CONSTANT_NAMES:
+            if suffix == "export" and prefix not in _CONSTANT_NAMES:
                 files.append(prefix)
         states = [file for file in files if f"{file}_adjoint_state" in files]
         controls = [file for file in files if f"stage_{file}" in files]
@@ -59,7 +59,7 @@ class BOCOPSolution:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.working_directory_filename})"
 
-    def _file_to_array(self, filename: str) -> np.ndarray:
+    def file_to_array(self, filename: str) -> np.ndarray:
         """Read a file in the directory, and return it's contents as a numpy array.
 
         Parameters
